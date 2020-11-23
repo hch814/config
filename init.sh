4bf86f080b526d6c1f9933f0750298b2ca884e4a
hch <617320696@qq.com>
2020-11-23 23:03:04 +0800

    feat(init.sh): zsh config setup

--- init.sh
+++ init.sh
@@ -10,10 +10,38 @@ function main() {
 
     check_or_create_dir $CONFIG_DIR
     setup_git_config $CONFIG_DIR $REPO_DIR
+    setup_zsh_config $CONFIG_DIR $REPO_DIR
+
+}
+
+# todo test
+setup_zsh_config(){
+    CONFIG_DIR=$1
+    REPO_DIR=$2
+    if [ ! -d $REPO_DIR/zsh/ohmyzsh/custom ]; then
+        cd $REPO_DIR
+        git submodule init
+        git submodule update
+    fi 
+    cd $REPO_DIR/zsh/ohmyzsh/custom/themes
+    [[ -L powerlevel10k || -d powerlevel10k ]] && trash_and_log `pwd`/powerlevel10k
+    ln -s ../../../powerlevel10k .
+
+    cd $REPO_DIR/zsh/ohmyzsh/custom/plugins
+    [[ -L zsh-autosuggestions || -d zsh-autosuggestions ]] && trash_and_log `pwd`/zsh-autosuggestions
+    ln -s ../../../zsh-autosuggestions .
+    [[ -L zsh-syntax-highlighting || -d zsh-syntax-highlighting ]] && trash_and_log `pwd`/zsh-syntax-highlighting
+    ln -s ../../../zsh-syntax-highlighting .
+
+    cd $CONFIG_DIR
+    ln -s $REPO_DIR/zsh/ $CONFIG_DIR/zsh
+
+    [ -f $HOME/.zshrc ] && trash_and_log $HOME/.zshrc
+    echo 'source $HOME/.config/zsh/zshrc' > $HOME/.zshrc
 }
 
 # todo
-setup_zsh_config(){
+setup_git_config(){
     return
 }
 
@@ -27,12 +55,14 @@ setup_git_config(){
     [ -d $CONFIG_DIR/git ] && trash_and_log $CONFIG_DIR/git
     ln -s $REPO_DIR/git $CONFIG_DIR/git
     git config --global user.name `whoami`
-    echo -e "`color green`######################## GIT ########################"
+    color green
+    echo "######################## GIT ########################"
     echo "# git配置以完成："
     echo "`git config -l --global`"
     echo "# 你可以根据需要修改用户名或邮箱："
     echo "# git config --global [user.email | user.name] <info>"
-    echo -e "######################## GIT ########################`color none`"
+    echo "######################## GIT ########################"
+    color none
 }
 
 check_or_create_dir() {
@@ -61,9 +91,11 @@ trash_and_log(){
     if [ ! -f $LOG_FILE ]; then
         msg="@@为了使配置生效，将影响配置的原有文件放入此回收桶(trash目录)中，你可以根据该回收日志(README.txt)对这些文件进行进一步处理，例如清除或回滚@@"
         echo $msg >> $LOG_FILE
-        echo -e "`color yellow`$msg`color none`"
+        color yellow
+        echo $msg
+        color none
     fi
-    if [ -f $TO ] || [ -d $TO ]; then
+    if [[ -e $TO || -L $TO ]]; then
         TO="$TO.`date +"%Y%m%d_%H%M%S"`"
     fi 
     mv $TRASH_FILE $TO
@@ -72,10 +104,10 @@ trash_and_log(){
 
 color(){
     case $1 in
-    'red') echo "\033[31m";;
-    'green') echo "\033[32m";;
-    'yellow') echo "\033[33m";;
-    *) echo "\033[m";;
+    'red') echo -e "\033[31m";;
+    'green') echo -e "\033[32m";;
+    'yellow') echo -e "\033[33m";;
+    *) echo -e "\033[m";;
     esac
 }
 
